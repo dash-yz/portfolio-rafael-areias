6210924e5e9f919d4986ec21dfacef27d16f2953
Vitor Cecato <179887077+dash-yz@users.noreply.github.com>
2026-01-11 16:52:00 -0300

    base

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,24 @@
+// Estilização
+  import './App.css'
+//.
+
+
+function App() {
+
+  return (
+    <>
+      <section className='sc-body'>
+        <header className='st-header'>
+          <h1>Rafael Areias</h1>
+          <nav className='st-nav'>
+            <h3>Portfólio</h3>
+            <h3>Sobre</h3>
+            <h3>Contato</h3>
+          </nav>
+        </header>
+      </section>
+    </>
+  )
+}
+
+export default App
